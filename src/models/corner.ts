73b0018f62fbe39d54ab9905215c0d51e10030b8
mino <sh00283ml@SH00283MLdeMacBook-Pro.local>
2022-07-25 00:11:10 +0800

    抽象函数

--- src/models/corner.ts
+++ src/models/corner.ts
@@ -4,10 +4,11 @@ import * as Graphics from '../graphcs/basicGraphics'
 import {BaseModel} from './basemodel'
 import {Direction, Position} from '../helpers/metrics'
 
+type WallContainer = [number,Wall][];
 export class Corner implements BaseModel<Corner> {
 
-  public startWalls: [number,Wall][]
-  public endWalls: [number,Wall][]
+  public startWalls: WallContainer
+  public endWalls: WallContainer
   private _positon: Position;
   isSelected: boolean
    constructor(pos: Position){
@@ -23,29 +24,35 @@ export class Corner implements BaseModel<Corner> {
    addStartWall(wall:Wall){
     // 获取到相邻位置的wall,修改wall edge的start值
     const deg = wall.direction.angle();
+     const index = this.findIndex(this.startWalls,deg);
+     //todo 增加其他的操作 找到前后的 wall 修改值
+    this.startWalls.splice(index,0 ,[deg,wall]);
+    
+   }
+
+   private findIndex(arr:WallContainer ,deg: number):number{
     let index = 0;
-    const len = this.startWalls.length;
+    const len = arr.length;
      for(let i= 0; i< len -1; i++){
         if (i== 0){
-         if(deg < this.startWalls[i][0]){
+         if(deg < arr[i][0]){
             break;
          }
         }else if (i == len - 1){
-            if(deg > this.startWalls[i][0]){
+            if(deg > arr[i][0]){
                 index = len;
                 break;
             }
         }
-       if(deg <= this.startWalls[i][0] && deg > this.startWalls[i-1][0]){
+       if(deg <= arr[i][0] && deg > arr[i-1][0]){
             index = i;
             break;
            } 
         
      }
-     //todo 增加其他的操作 找到前后的 wall 修改值
-    this.startWalls.splice(index,0 ,[deg,wall]);
-    
+     return index;
    }
+
    
    addEndWall(wall:Wall){
     this.endWalls?.push([0,wall]);
